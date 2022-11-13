8164291e8013a8d01409faa9a2fc4dc01fff1967
Zeus-chen <qhd19850062726@gmail.com>
2022-11-13 07:33:10 -0800

    Updates

---
+++ precache-manifest.d6485e67ef940fe30df9764955e04d22.js
@@ -0,0 +1,330 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa2166a7621a7be9094cbdf46efca37",
+    "url": "/ZeusSwap/index.html"
+  },
+  {
+    "revision": "77e7c5a709fb58474162",
+    "url": "/ZeusSwap/static/css/4.d7a5a61f.chunk.css"
+  },
+  {
+    "revision": "921c508e6b1a83f681eb",
+    "url": "/ZeusSwap/static/js/0.7b2b5fe3.chunk.js"
+  },
+  {
+    "revision": "7eb8ecf91d4d9d809462",
+    "url": "/ZeusSwap/static/js/1.7ff3614b.chunk.js"
+  },
+  {
+    "revision": "77e7c5a709fb58474162",
+    "url": "/ZeusSwap/static/js/4.775b0d08.chunk.js"
+  },
+  {
+    "revision": "5f9b6e7e0c34704d492dae42cf9cac81",
+    "url": "/ZeusSwap/static/js/4.775b0d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e18fd7bfa0885935ce",
+    "url": "/ZeusSwap/static/js/5.4878be0c.chunk.js"
+  },
+  {
+    "revision": "caaa9c24a9c77eaead93960f555b5a26",
+    "url": "/ZeusSwap/static/js/5.4878be0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89b14d4dd076aa42db3",
+    "url": "/ZeusSwap/static/js/6.4dc38d4d.chunk.js"
+  },
+  {
+    "revision": "25732e178816561c277d",
+    "url": "/ZeusSwap/static/js/7.2f0f0a5c.chunk.js"
+  },
+  {
+    "revision": "ef998acd8f65ae6b9b420793d7484b0e",
+    "url": "/ZeusSwap/static/js/7.2f0f0a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bde305af4d2faf26b68",
+    "url": "/ZeusSwap/static/js/8.6487b237.chunk.js"
+  },
+  {
+    "revision": "84affba743f64c3e5936",
+    "url": "/ZeusSwap/static/js/9.daae23d8.chunk.js"
+  },
+  {
+    "revision": "85d1e63c01e1d5cc1f1c",
+    "url": "/ZeusSwap/static/js/main.02145210.chunk.js"
+  },
+  {
+    "revision": "2223e92d81eab13f025e",
+    "url": "/ZeusSwap/static/js/runtime-main.0ba59e6b.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/ZeusSwap/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/ZeusSwap/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "07e69b53f8cf91da94096d1abf484302",
+    "url": "/ZeusSwap/static/media/Inter-BlackItalic.07e69b53.woff"
+  },
+  {
+    "revision": "daa1ca3cebd42cf5ebd53d9a6942b2cf",
+    "url": "/ZeusSwap/static/media/Inter-BlackItalic.daa1ca3c.woff2"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/ZeusSwap/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/ZeusSwap/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "8ef77a031c5b96e53f2eeeb009232181",
+    "url": "/ZeusSwap/static/media/Inter-BoldItalic.8ef77a03.woff2"
+  },
+  {
+    "revision": "e0879d64763e509f309940120e7d1f5a",
+    "url": "/ZeusSwap/static/media/Inter-BoldItalic.e0879d64.woff"
+  },
+  {
+    "revision": "38bc51bccb7dd07b2752c11888cbdc89",
+    "url": "/ZeusSwap/static/media/Inter-ExtraBold.38bc51bc.woff"
+  },
+  {
+    "revision": "92d16aee8fb5f5c5cfd660b2d07e1148",
+    "url": "/ZeusSwap/static/media/Inter-ExtraBold.92d16aee.woff2"
+  },
+  {
+    "revision": "0e4b21ebad8d568078450713ee9339cf",
+    "url": "/ZeusSwap/static/media/Inter-ExtraBoldItalic.0e4b21eb.woff"
+  },
+  {
+    "revision": "57ea76d011f6fff8cb7013b783bad58e",
+    "url": "/ZeusSwap/static/media/Inter-ExtraBoldItalic.57ea76d0.woff2"
+  },
+  {
+    "revision": "4bd040df09ea4765148b5743b8493e5f",
+    "url": "/ZeusSwap/static/media/Inter-ExtraLight.4bd040df.woff"
+  },
+  {
+    "revision": "4d9f96f8504cd034ef7e5f25434ca550",
+    "url": "/ZeusSwap/static/media/Inter-ExtraLight.4d9f96f8.woff2"
+  },
+  {
+    "revision": "54d3d9a59ea904ebd11c3ac308893bfb",
+    "url": "/ZeusSwap/static/media/Inter-ExtraLightItalic.54d3d9a5.woff2"
+  },
+  {
+    "revision": "84c26656e02913a54131fc64431212ae",
+    "url": "/ZeusSwap/static/media/Inter-ExtraLightItalic.84c26656.woff"
+  },
+  {
+    "revision": "9528384cf7aebfa81a198626cb05f5e8",
+    "url": "/ZeusSwap/static/media/Inter-Italic.9528384c.woff2"
+  },
+  {
+    "revision": "e4ad3666e223c64e4df963cec740bf09",
+    "url": "/ZeusSwap/static/media/Inter-Italic.e4ad3666.woff"
+  },
+  {
+    "revision": "5baca21acf845c8e746f08675f40300b",
+    "url": "/ZeusSwap/static/media/Inter-Light.5baca21a.woff2"
+  },
+  {
+    "revision": "b9920de0749addeb5a53462fb5bbe070",
+    "url": "/ZeusSwap/static/media/Inter-Light.b9920de0.woff"
+  },
+  {
+    "revision": "0555a46cda7fc3406a875d6bacf8fcff",
+    "url": "/ZeusSwap/static/media/Inter-LightItalic.0555a46c.woff"
+  },
+  {
+    "revision": "adc70179a9e60e14f2a4f7d106166004",
+    "url": "/ZeusSwap/static/media/Inter-LightItalic.adc70179.woff2"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/ZeusSwap/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/ZeusSwap/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "417907d2be56d39b2a9c0b52fc8a00e8",
+    "url": "/ZeusSwap/static/media/Inter-MediumItalic.417907d2.woff"
+  },
+  {
+    "revision": "565a7104c497ccb1bf12832d8b341861",
+    "url": "/ZeusSwap/static/media/Inter-MediumItalic.565a7104.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/ZeusSwap/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/ZeusSwap/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "1db6c55ca0f2a97fbce3b0b3903a7bb7",
+    "url": "/ZeusSwap/static/media/Inter-SemiBold.1db6c55c.woff"
+  },
+  {
+    "revision": "dd8a55ef7058cdaeb96ef9fc65344726",
+    "url": "/ZeusSwap/static/media/Inter-SemiBold.dd8a55ef.woff2"
+  },
+  {
+    "revision": "81678d1a50a895cc1232f3e287976083",
+    "url": "/ZeusSwap/static/media/Inter-SemiBoldItalic.81678d1a.woff"
+  },
+  {
+    "revision": "ac201e30486307c75c5038f61df9fd9f",
+    "url": "/ZeusSwap/static/media/Inter-SemiBoldItalic.ac201e30.woff2"
+  },
+  {
+    "revision": "850febbe5e8a7625c5603061d2e7c0ce",
+    "url": "/ZeusSwap/static/media/Inter-Thin.850febbe.woff2"
+  },
+  {
+    "revision": "ead42837911f0b55b0877c12b50d9157",
+    "url": "/ZeusSwap/static/media/Inter-Thin.ead42837.woff"
+  },
+  {
+    "revision": "a76db06556290ed3bd198a72b24d8006",
+    "url": "/ZeusSwap/static/media/Inter-ThinItalic.a76db065.woff"
+  },
+  {
+    "revision": "e08d9b2a5079b81cc39804c2a7406254",
+    "url": "/ZeusSwap/static/media/Inter-ThinItalic.e08d9b2a.woff2"
+  },
+  {
+    "revision": "2690e3c25733fb852b55b3f8d651a431",
+    "url": "/ZeusSwap/static/media/Inter-italic.var.2690e3c2.woff2"
+  },
+  {
+    "revision": "90e8f61d26f65b5ff0acc45ddf6740ea",
+    "url": "/ZeusSwap/static/media/Inter-roman.var.90e8f61d.woff2"
+  },
+  {
+    "revision": "4b976905f2be1cb9201f5d94afd9edd2",
+    "url": "/ZeusSwap/static/media/Inter.var.4b976905.woff2"
+  },
+  {
+    "revision": "cd061363bbf9cd7a26cb09b642dcaf63",
+    "url": "/ZeusSwap/static/media/arrow-down-blue.cd061363.svg"
+  },
+  {
+    "revision": "c0dedd2f2ed0c4d07d7ca75af3f0a65f",
+    "url": "/ZeusSwap/static/media/arrow-down-grey.c0dedd2f.svg"
+  },
+  {
+    "revision": "337ad716bd89163e2a9c3495b7e0f029",
+    "url": "/ZeusSwap/static/media/arrow-right-white.337ad716.png"
+  },
+  {
+    "revision": "d285b6cf22b4f1552bb4009333462632",
+    "url": "/ZeusSwap/static/media/arrow-right.d285b6cf.svg"
+  },
+  {
+    "revision": "904b44c2b22eb2d49f618396e6f2db1a",
+    "url": "/ZeusSwap/static/media/blue-loader.904b44c2.svg"
+  },
+  {
+    "revision": "ed2a1dad16cb9a4b9afd788ddaae7290",
+    "url": "/ZeusSwap/static/media/circle-grey.ed2a1dad.svg"
+  },
+  {
+    "revision": "2d975615c4c409c3b6b00e8ae7c5767a",
+    "url": "/ZeusSwap/static/media/circle.2d975615.svg"
+  },
+  {
+    "revision": "62578f5994645a1825d4829e2c4394b0",
+    "url": "/ZeusSwap/static/media/coinbaseWalletIcon.62578f59.svg"
+  },
+  {
+    "revision": "b20914ec5482543a0b1b2c6d5509ab96",
+    "url": "/ZeusSwap/static/media/dropdown-blue.b20914ec.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "/ZeusSwap/static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "b96d70e10dd30a64a0d122603577c8ae",
+    "url": "/ZeusSwap/static/media/dropup-blue.b96d70e1.svg"
+  },
+  {
+    "revision": "50c67f3cdd04281013ef95e92fc7244e",
+    "url": "/ZeusSwap/static/media/link.50c67f3c.svg"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "/ZeusSwap/static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "/ZeusSwap/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "/ZeusSwap/static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "4f2c4440e19ed9428d0caae5d9840df6",
+    "url": "/ZeusSwap/static/media/menu.4f2c4440.svg"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "/ZeusSwap/static/media/metamask.023762b6.png"
+  },
+  {
+    "revision": "e8021e51723d455b2f9fa2446808d2a3",
+    "url": "/ZeusSwap/static/media/plus-blue.e8021e51.svg"
+  },
+  {
+    "revision": "d8e0be7d6efb0b53c37eb75e44b35bda",
+    "url": "/ZeusSwap/static/media/plus-grey.d8e0be7d.svg"
+  },
+  {
+    "revision": "b234b2bfa0417c7e8711c3a8d17afeec",
+    "url": "/ZeusSwap/static/media/portisIcon.b234b2bf.png"
+  },
+  {
+    "revision": "1ae4d9f4653371789d98b85139933d27",
+    "url": "/ZeusSwap/static/media/question-mark.1ae4d9f4.svg"
+  },
+  {
+    "revision": "a46e8bc1a36444be83a85007353d692f",
+    "url": "/ZeusSwap/static/media/question.a46e8bc1.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/ZeusSwap/static/media/spinner.be00fc4a.svg"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "/ZeusSwap/static/media/trustWallet.edcc1ab5.png"
+  },
+  {
+    "revision": "8215855c185176eb79446ce8cc1f3998",
+    "url": "/ZeusSwap/static/media/walletConnectIcon.8215855c.svg"
+  },
+  {
+    "revision": "0fdb877dc097b1579f044318c07be7da",
+    "url": "/ZeusSwap/static/media/wordmark.0fdb877d.svg"
+  },
+  {
+    "revision": "d04811bb2886e005e4f6d9c826d4a0ff",
+    "url": "/ZeusSwap/static/media/wordmark_white.d04811bb.svg"
+  },
+  {
+    "revision": "5b8e218668bfea1d44b887bd042f6a52",
+    "url": "/ZeusSwap/static/media/x.5b8e2186.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ZeusSwap/precache-manifest.d6485e67ef940fe30df9764955e04d22.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZeusSwap/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
